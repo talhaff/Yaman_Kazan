b10e7d781afa52c6bfa0c8738edde3705331a8fe
talhaff <talhaozcanf@gmail.com>
2026-05-08 16:17:13 +0300

    feat: implement dynamic Sanity Studio page for tool configuration

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,5 +1,10 @@
-import { NextStudio } from "next-sanity/studio";
+import dynamic from "next/dynamic";
 import config from "../../../../sanity.config";
+
+const NextStudio = dynamic(
+  () => import("next-sanity/studio").then((mod) => mod.NextStudio),
+  { ssr: false }
+);
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
